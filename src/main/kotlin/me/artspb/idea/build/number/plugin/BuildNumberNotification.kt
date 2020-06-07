abecd7e6244f0ce6ac9096b0e6d02873b5ca9b8b
Artem Khvastunov <artem.khvastunov@jetbrains.com>
2020-06-07 15:48:42 +0200

    Implement Copy About Info to Clipboard action

---
+++ src/main/kotlin/me/artspb/idea/build/number/plugin/BuildNumberNotification.kt
@@ -0,0 +1,14 @@
+package me.artspb.idea.build.number.plugin
+
+import com.intellij.notification.NotificationGroup
+import com.intellij.notification.NotificationType
+import com.intellij.notification.Notifications
+
+object BuildNumberNotification {
+
+    private val notificationGroup = NotificationGroup.balloonGroup("me.artspb.idea.build.number.plugin.BuildNumberNotification")
+
+    fun info(title: String, content: String) {
+        Notifications.Bus.notify(notificationGroup.createNotification(title, content, NotificationType.INFORMATION, null))
+    }
+}
